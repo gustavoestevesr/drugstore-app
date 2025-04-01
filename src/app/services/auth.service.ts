21e1a97943e24c7831eb1789c8627db89e0d4791
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-03-31 21:36:04 -0300

    feat: isLogged added to the auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,27 +1,52 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IUser } from '../models/user.model';
 import { delay, map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   // Variables
-  private API_URL = 'http://localhost:3000/users';
+  private readonly API_URL = 'http://localhost:3000/users';
+  private readonly isLogged = signal(false);
 
   // Services
   private http = inject(HttpClient);
+
+  constructor() {
+    this.isLogged = signal<boolean>(this.getIsLoggedLocalStorage());
+  }
 
   // Methods
   login(user: IUser) {
     return this.http.get<IUser[]>(this.API_URL).pipe(
       delay(1000),
-      map((users: IUser[]) => users.find((u) => u.email === user.email && u.password === user.password) || null)
+      map((users: IUser[]) => {
+        const foundUser = users.find((u) => u.email === user.email && u.password === user.password);
+        if (foundUser) this.setIsLoggedLocalStorage(true);
+        return foundUser || null;
+      })
     );
   }
 
   register(user: IUser) {
     return this.http.post(this.API_URL, user).pipe(delay(1000));
+  }
+
+  // Getters
+  getIsLogged() {
+    return this.isLogged();
+  }
+
+  // LocalStorage
+  setIsLoggedLocalStorage(value: boolean) {
+    localStorage.setItem('isLogged', JSON.stringify(value));
+    this.isLogged.set(value);
+  }
+
+  getIsLoggedLocalStorage(): boolean {
+    const stored = localStorage.getItem('isLogged');
+    return stored ? JSON.parse(stored) : false;
   }
 }
